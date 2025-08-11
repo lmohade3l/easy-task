5b2b9abb6dbccb9ebaad507e19c4343cabee1609
unknown <mohade3.2710@gmail.com>
2025-08-11 17:56:55 +0330

    feat: added users list

--- src/app/users/user.component.ts
+++ src/app/users/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  @Input() user!: {
+    id: string;
+    avatar: string;
+    name: string
+  }
 }
