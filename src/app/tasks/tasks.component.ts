01356e810a539f4a732650d4338b3e768e259b07
Mohadese <mohade3.2710@gmail.com>
2025-08-16 22:33:07 +0330

    feat: add some dummy tasks and list them

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../models/user';
 import { TaskComponent } from "../task/task.component";
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-tasks',
@@ -10,5 +11,34 @@ import { TaskComponent } from "../task/task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() user!: User
+  @Input({ required: true }) user!: User
+  dummyTasks: Task[] = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ]
+
+  get selectedUserTasks() {
+    return this.dummyTasks.filter(t => t.userId === this.user?.id)
+  }
 }
