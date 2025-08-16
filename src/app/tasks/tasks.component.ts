b8394529a7920282fd7fc0a5de14721f4feca2d9
Mohadese <mohadese@Mohadeses-MacBook-Air.local>
2025-08-16 22:21:44 +0330

    feat: add task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../models/user';
+import { TaskComponent } from "../task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
