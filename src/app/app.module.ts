a563e407520ab66bebc9b7e9132438d6861a7985
Mohadese <mohade3.2710@gmail.com>
2025-08-18 19:18:22 +0330

    feat: implment with module components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './users/user.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, TaskComponent, TasksComponent, UserComponent, HeaderComponent]
 })
 export class AppModule {
 
