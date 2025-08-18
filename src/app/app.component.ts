20fa3908a024fab66ac51c65503f34ecf5e908d8
Mohadese <mohade3.2710@gmail.com>
2025-08-18 15:18:15 +0330

    set up the project base to be modular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  // standalone: true,
+  // imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
